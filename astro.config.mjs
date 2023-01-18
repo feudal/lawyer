515626ce0a22c29dcc6091ca446270dffd159399
Dmitri Calmîș <calmis87@gmail.com>
2023-01-18 12:12:00 +0200

    prepare for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@import "src/styles/variables.scss";',
-        },
-      },
-    },
+          additionalData: '@import "src/styles/variables.scss";'
+        }
+      }
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
