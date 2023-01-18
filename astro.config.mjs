c4775ff346aac45702e59944a3577130d32a9c7c
Dmitri Calmîș <calmis87@gmail.com>
2023-01-18 13:44:01 +0200

    fix styles error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@import "src/styles/variables.scss";'
-        }
-      }
-    }
+          additionalData: '@import "src/styles/variables.scss";',
+        },
+      },
+    },
   },
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
